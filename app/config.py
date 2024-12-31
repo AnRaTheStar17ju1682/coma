a0483e1a3cb4fa2aa2a5e419a1e7810da2e11892
AnRaTheStar17ju1682 <outworlddevourer1337darklordking666@outlook.com>
2024-12-31 20:23:43 +0300

    added db, still just creating a general shape of the project

---
+++ app/config.py
@@ -0,0 +1,22 @@
+from pydantic import computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+    
+    
+    @computed_field
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
---
+++ app/database/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncEngine, AsyncSession
+
+from ..config import settings
+
+
+database_url = settings.DATABASE_URL
+
+
+engine = create_async_engine(url=database_url, echo=True)
+factory = async_sessionmaker(engine)
+
+
+class Base(DeclarativeBase):
+    pass
