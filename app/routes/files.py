db8ece15d4ad9a2c4d3f6c2e6abf0362ec23db78
AnRaTheStar17ju1682 <outworlddevourer1337darklordking666@outlook.com>
2025-01-25 19:09:21 +0300

    Added file existence check before returning FileResponse to prevent runtime errors

--- app/routes/files.py
+++ app/routes/files.py
@@ -1,17 +1,31 @@
-from fastapi import APIRouter
+from pathlib import Path
+
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import FileResponse
 
+from config import settings
 
+
+content_path = settings.CONTENT_DIR
 router = APIRouter(prefix="/files", tags=["files"])
+
+
+def return_if_exists(file_path: str):
+    file_path = Path(file_path)
+    
+    if file_path.exists():
+        return FileResponse(file_path, media_type="image/webp")  
+    else:
+        raise HTTPException(status_code=404, detail="File not found")
 
 
 @router.get("/item/{item_hash}", status_code=200, response_class=FileResponse)
 async def download_item(item_hash: str):
-    file_path = f"./content/{item_hash}.webp"
-    return FileResponse(file_path, media_type="image/webp")
+    file_path = f"{content_path}/{item_hash}.webp"
+    return return_if_exists(file_path)
 
 
 @router.get("/thumbnail/{item_hash}", status_code=200, response_class=FileResponse)
 async def download_thumbnail(item_hash: str):
-    file_path = f"./content/thumbnails/thumbnail_{item_hash}.webp"
-    return FileResponse(file_path, media_type="image/webp")
+    file_path = f"{content_path}/thumbnails/thumbnail_{item_hash}.webp"
+    return return_if_exists(file_path)
